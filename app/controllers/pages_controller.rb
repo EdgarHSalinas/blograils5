1a35060f8033d7374462887192b2e34ec92c6679
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-06-25 15:41:42 -0700

    Added a way to both create and show new Pages requiring page object and permit :title, :body, :slug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,24 @@ class PagesController < ApplicationController
   def index
     @pages = Page.all
   end 
+
+  def show 
+    @page = Page.find(params[:id])
+    # page object is return and gets assigned to the @page instance
+    # render plain: @page.title => Used to test its working in browser
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create 
+    # render plain: params.to_json ==> Used to fist test 
+    page_params = params.require(:page).permit(:title, :body, :slug)
+    @page = Page.new(page_params)
+    @page.save 
+    redirect_to @page
+
+  end
 end
 
