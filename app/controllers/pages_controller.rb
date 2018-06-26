1da6cfe4cd6512079bb0652708e8787af839b310
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-06-25 17:13:58 -0700

    refactor of PagesController before_action and Routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @pages = Page.all
   end 
 
   def show 
-    @page = Page.find(params[:id])
+    # @page = Page.find(params[:id]) ==> set_page method ==> before_action
     # page object is return and gets assigned to the @page instance
     # render plain: @page.title => Used to test its working in browser
   end
@@ -21,19 +23,26 @@ class PagesController < ApplicationController
   end
 
  def edit 
-   @page = Page.find(params[:id])
  end
 
  def update 
-   @page = Page.find(params[:id])
    @page.update(page_params)
    redirect_to @page
  end
+
+ def destroy
+   @page.destroy
+   redirect_to pages_path
+ end
+ 
  
  private 
     def page_params
       params.require(:page).permit(:title, :body, :slug)
     end
 
+    def set_page
+      @page = Page.find(params[:id])
+    end 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to: 'pages#index'
-  post '/pages', to: 'pages#create'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show', as: 'page' # id always last ?
-  get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
-  patch '/pages/:id', to: 'pages#update'
+  resources :pages 
+
+  # 7 lines below get converted to 1 line above 
+  
+  # get '/pages', to: 'pages#index'
+  # post '/pages', to: 'pages#create'
+  # get '/pages/new', to: 'pages#new', as: 'new_page'
+  # get '/pages/:id', to: 'pages#show', as: 'page' # id always last ?
+  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch '/pages/:id', to: 'pages#update'
+  # delete '/pages/:id', to: 'pages#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
